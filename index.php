7dc1d76fbc2a9ebd9910cbec8dc782da0262a1e1
GSquadron <adishnica@yahoo.com>
2018-06-03 20:38:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Tic Tac Toe</title>
+		<link rel = "stylesheet" href = "css/default.css">
+	</head>
+	<body>
+		<section class = "container">
+			<h3 id = "gameOver"></h3>
+			<section class = "box-container">
+				<?php for($i = 0; $i < 9; $i++){ ?>
+
+					<div></div>
+	
+				<?php } ?>
+			</section>
+		</section>
+
+		<script src = "js/default.js"></script>
+	</body>
+</html>
